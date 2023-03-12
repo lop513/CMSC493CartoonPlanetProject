89ddc19f0b5119f96d119caa00e3705166bab243
lop513 <fishpolon@gmail.com>
2023-03-12 15:24:08 -0400

    Level Update 3/12

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,10 +12,16 @@ public class Spawner : MonoBehaviour
 {
     private Pathfinder pf;
 
+    public int xSpawnLoc = 0;
+    public int zSpawnLoc = 0;
+
+    //public List<Vector2Int> spawnLocation = pf.spawn_candidates.OrderBy(x => xSpawnLoc).Take(swarm.Length).ToList();
     public GameObject enemyPrefab;
     private GameObject[] swarm;
     public int SWARM_SIZE = 1;
     public float SWARM_TICK = 120;
+
+    public Vector2Int[] spawns;
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +40,18 @@ public class Spawner : MonoBehaviour
     //Wait for path vectors to update before step
     void LateUpdate()
     {
+        Vector2Int[] spawns = new Vector2Int[] {
+                new Vector2Int(xSpawnLoc, zSpawnLoc)
+        };
         if (swarm == null) //weird synchronization tomfoolery
         {
             swarm = new GameObject[SWARM_SIZE];
-            List<Vector2Int> spawns = pf.spawn_candidates.OrderBy(x => UnityEngine.Random.value).Take(swarm.Length).ToList();
+            //List<Vector2Int> spawns; // = pf.spawn_candidates.OrderBy(x => xSpawnLoc).Take(swarm.Length).ToList();
+
+            spawns = new Vector2Int[] {
+                new Vector2Int(xSpawnLoc, zSpawnLoc)
+            };
+
             for (int i = 0; i < swarm.Length; i++) { 
                 swarm[i] = Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity);
                 Enemy script = swarm[i].AddComponent<Enemy>();
